b270c36230b4b4f5f5e369ca0654f6c8efef398a
jitbit <jitbit@gmail.com>
2016-01-18 22:18:56 +0300

    some ui fixes

--- VSUnusedImg/FindImgsForm.Designer.cs
+++ VSUnusedImg/FindImgsForm.Designer.cs
@@ -46,18 +46,21 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.lstImages.FormattingEnabled = true;
-			this.lstImages.Location = new System.Drawing.Point(12, 74);
+			this.lstImages.ItemHeight = 20;
+			this.lstImages.Location = new System.Drawing.Point(18, 114);
+			this.lstImages.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.lstImages.Name = "lstImages";
 			this.lstImages.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-			this.lstImages.Size = new System.Drawing.Size(333, 134);
+			this.lstImages.Size = new System.Drawing.Size(498, 204);
 			this.lstImages.TabIndex = 0;
 			this.lstImages.SelectedIndexChanged += new System.EventHandler(this.lstImages_SelectedIndexChanged);
 			// 
 			// btnStart
 			// 
-			this.btnStart.Location = new System.Drawing.Point(12, 12);
+			this.btnStart.Location = new System.Drawing.Point(18, 18);
+			this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btnStart.Name = "btnStart";
-			this.btnStart.Size = new System.Drawing.Size(75, 23);
+			this.btnStart.Size = new System.Drawing.Size(112, 35);
 			this.btnStart.TabIndex = 1;
 			this.btnStart.Text = "Search!";
 			this.btnStart.UseVisualStyleBackColor = true;
@@ -65,43 +68,50 @@
 			// 
 			// progressBar1
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(93, 12);
+			this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar1.Location = new System.Drawing.Point(140, 18);
+			this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(252, 23);
+			this.progressBar1.Size = new System.Drawing.Size(378, 35);
 			this.progressBar1.TabIndex = 2;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(13, 55);
+			this.label1.Location = new System.Drawing.Point(20, 85);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(83, 13);
+			this.label1.Size = new System.Drawing.Size(124, 20);
 			this.label1.TabIndex = 3;
 			this.label1.Text = "Unused images:";
 			// 
 			// statusStrip1
 			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
 			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-			this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+			this.statusStrip1.Location = new System.Drawing.Point(0, 373);
 			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(357, 22);
+			this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+			this.statusStrip1.Size = new System.Drawing.Size(536, 30);
 			this.statusStrip1.TabIndex = 4;
 			this.statusStrip1.Text = "statusStrip1";
 			// 
 			// toolStripStatusLabel1
 			// 
 			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-			this.toolStripStatusLabel1.Size = new System.Drawing.Size(48, 17);
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(72, 25);
 			this.toolStripStatusLabel1.Text = "Ready...";
 			// 
 			// btnDelete
 			// 
 			this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnDelete.Enabled = false;
-			this.btnDelete.Location = new System.Drawing.Point(190, 214);
+			this.btnDelete.Location = new System.Drawing.Point(285, 329);
+			this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btnDelete.Name = "btnDelete";
-			this.btnDelete.Size = new System.Drawing.Size(155, 23);
+			this.btnDelete.Size = new System.Drawing.Size(232, 35);
 			this.btnDelete.TabIndex = 5;
 			this.btnDelete.Text = "Delete all unused images";
 			this.btnDelete.UseVisualStyleBackColor = true;
@@ -109,10 +119,12 @@
 			// 
 			// btnDeleteSelected
 			// 
+			this.btnDeleteSelected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnDeleteSelected.Enabled = false;
-			this.btnDeleteSelected.Location = new System.Drawing.Point(12, 214);
+			this.btnDeleteSelected.Location = new System.Drawing.Point(18, 329);
+			this.btnDeleteSelected.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.btnDeleteSelected.Name = "btnDeleteSelected";
-			this.btnDeleteSelected.Size = new System.Drawing.Size(172, 23);
+			this.btnDeleteSelected.Size = new System.Drawing.Size(258, 35);
 			this.btnDeleteSelected.TabIndex = 6;
 			this.btnDeleteSelected.Text = "Delete selected";
 			this.btnDeleteSelected.UseVisualStyleBackColor = true;
@@ -120,9 +132,9 @@
 			// 
 			// FindImgsForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(357, 262);
+			this.ClientSize = new System.Drawing.Size(536, 403);
 			this.Controls.Add(this.btnDeleteSelected);
 			this.Controls.Add(this.btnDelete);
 			this.Controls.Add(this.statusStrip1);
@@ -131,6 +143,7 @@
 			this.Controls.Add(this.btnStart);
 			this.Controls.Add(this.lstImages);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "FindImgsForm";
